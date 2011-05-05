f3ad7bb67810a4c110f1900f38b203d36e030b1f
Jameel Haffejee <jameel@republiccommandos.co.za>
2011-05-05 09:25:21 +0200

    Added smallest chunk checker

--- spacemanager/views.py
+++ spacemanager/views.py
@@ -9,10 +9,28 @@ def getSpace(disk):
     used = s.f_bsize * (s.f_blocks - s.f_bavail)
     return (available / (1048576)) / 1000
 
+def calcSize(start_path):
+	total_size = 0
+	for dirpath, dirnames, filenames in os.walk(start_path):
+	    for f in filenames:
+		fp = os.path.join(dirpath, f)
+		total_size += os.path.getsize(fp)
+	return total_size / (1024 * 1024)
+
 def moveFiles(request):
     '''This defines the monitor and dump folders , later we can add multiple so that 
     it can monitor many folders and dump to many folders as needed based on space '''
     monitorFolder = {'folder':'/downloads/complete/TV/','type':'series'}
     dumpFolder = {'folder':'/media/DownloadWing/Series/','type':'series'}
     files = os.listdir(monitorFolder['folder'])
-    return HttpResponse(sum([os.path.getsize(f) for f in os.listdir(files[0]) if os.path.isfile(f)]))
+    files.sort()
+    smallestChunk = 9999999
+    foldername = ''
+    for folder in files:
+	myfolder = os.path.join(monitorFolder['folder']+folder) 
+	if os.path.isdir(myfolder):
+		if calcSize(myfolder) < smallestChunk:
+			smallestChunk = calcSize(myfolder)
+			foldername = myfolder
+		
+    return HttpResponse('This smallest folder that can be moved and its size is '+foldername+' ' +smallestChunk.__str__())
