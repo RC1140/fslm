a0be8afdd41944ba5b686d47b318e529e0b43f64
jameel <jameel@republiccommandos.co.za>
2011-05-05 21:57:38 +0200

    Added extra fields to models

--- spacemanager/models.py
+++ spacemanager/models.py
@@ -4,6 +4,17 @@ from django.contrib import admin
 class Drive(models.Model):
     Path=models.CharField(max_length=1024)
     Name=models.CharField(max_length=255)
+    Drive_Types = (
+        ('D','Dump'),
+        ('M','Monitor'),
+    )
+    DriveType = models.CharField(max_length=1,choices=Drive_Type) 
+    #For Dump drives we need to know maximum space usage as a %
+    MaxUsagePercentage = models.IntegerField()
+    '''For Monitor drives we need to know the ideal space usage as a % 
+       the app will try to reach this but depending on free space on 
+       the dump drives this might not be possible'''
+    IdealFreeSpacePercentage = models.IntegerField()
 
 class MediaType(models.Model):    
     Name=models.CharField(max_length=255)
