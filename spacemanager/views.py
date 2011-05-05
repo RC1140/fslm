8767908ff977317a50a62264fd26cf4552bca050
jameel <jameel@republiccommandos.co.za>
2011-05-05 08:48:30 +0200

    Added a few basic methods

--- spacemanager/views.py
+++ spacemanager/views.py
@@ -1,1 +1,18 @@
 # Create your views here.
+import os
+from django.http import HttpResponse
+
+def getSpace(disk):
+    s = os.statvfs(disk)
+    capacity = s.f_bsize * s.f_blocks
+    available = s.f_bsize * s.f_bavail
+    used = s.f_bsize * (s.f_blocks - s.f_bavail)
+    return (available / (1048576)) / 1000
+
+def moveFiles(request):
+    '''This defines the monitor and dump folders , later we can add multiple so that 
+    it can monitor many folders and dump to many folders as needed based on space '''
+    monitorFolder = {'folder':'/downloads/complete/TV/','type':'series'}
+    dumpFolder = {'folder':'/media/DownloadWing/Series/','type':'series'}
+    files = os.listdir(monitorFolder['folder'])
+    return HttpResponse(sum([os.path.getsize(f) for f in os.listdir(files[0]) if os.path.isfile(f)]))
--- urls.py
+++ urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     # Example:
     # (r'^fslm/', include('fslm.foo.urls')),
+     (r'^move/', 'spacemanager.views.moveFiles'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
