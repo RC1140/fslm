0c089a002edef4821a0e73561de5aab5fac27133
jameel <jameel@republiccommandos.co.za>
2011-05-22 21:12:29 +0200

    Working to fix stuff

--- spacemanager/views.py
+++ spacemanager/views.py
@@ -22,23 +22,25 @@ def calcSize(start_path):
             total_size += os.path.getsize(fp)
     return total_size / (1024 * 1024)
 
-def smallestFolder(request):
+def smallestFolder(folderToCheck):
     '''This defines the monitor and dump folders , later we can add multiple so that 
     it can monitor many folders and dump to many folders as needed based on space '''
-    monitorFolder = {'folder':'/downloads/complete/TV/','type':'series'}
-    dumpFolder = {'folder':'/media/DownloadWing/Series/','type':'series'}
-    files = os.listdir(monitorFolder['folder'])
+    files = os.listdir(folderToCheck)
     files.sort()
     smallestChunk = 9999999
     foldername = ''
-    for folder in files:
-	myfolder = os.path.join(monitorFolder['folder'],folder) 
+    for checkFile in files:
+	myfolder = os.path.join(folderToCheck,checkFile) 
 	if os.path.isdir(myfolder):
 		if calcSize(myfolder) < smallestChunk:
 			smallestChunk = calcSize(myfolder)
 			foldername = myfolder
 		
-    return HttpResponse('This smallest folder that can be moved and its size is '+foldername+' ' +smallestChunk.__str__())
+    if foldername != '':
+        return (foldername,smallestChunk)
+    else:
+        return ''
+    #return HttpResponse('This smallest folder that can be moved and its size is '+foldername+' ' +smallestChunk.__str__())
 
 def home(request, callbacks):
     return render_to_response('home.html', context_instance=RequestContext(request))
@@ -54,7 +56,7 @@ def drivesList(request, callbacks):
     driveData += ']'
     return HttpResponse(driveData)
 
-def getAvailableDumpFolder():
+def getFirstAvailableDumpFolder():
     '''Should return a folder where files can be moved to 
         if none is found then a '' is returned and nothing should be
         moved , it might also be good to indicate this somewhere in the
@@ -63,10 +65,15 @@ def getAvailableDumpFolder():
         return ''
 
     drives = Drive.objects.filter(DriveType='D').order_by('DumpPreference')
-    for drive in drives:
-       freespace = calcSize(drive.Path)
 
-
+    '''TODO : We need to indicate when a folder is full to prevent a drive
+    from being over used'''
+    if drives.count() > 0:
+        return drives[0].Path
+    else:
+        return ''
+    #for drive in drives:
+       #freespace = calcSize(drive.Path)
         
 def moveFiles(request):
     #This is a generic move request almost like what would happen in cron job
@@ -94,7 +101,10 @@ def moveFiles(request):
                     '''If the size of the folder we are checking is smaller than the amount of
                         space we need its ok to move the folder'''
                     if calcSize(myfolder) <= spaceToFree:
-                        moveFolderBackground.delay(myfolder,os.path.join(dumpFolder['folder'],folder))
-                        return HttpResponse('Moving in the background : '+myfolder)
+                        '''Get a dump folder , check if one is found if so that use it for the copies'''
+                        firstAvailableFolder = getFirstAvailableDumpFolder()
+                        if firstAvailableFolder != '':
+                            moveFolderBackground.delay(myfolder,os.path.join(firstAvailableFolder,folder))
+                            return HttpResponse('Moving in the background : '+myfolder)
 
     return HttpResponse('Nothing moved')
