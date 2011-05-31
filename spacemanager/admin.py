5d30d3cf311c8282ca09bd6218bdc1ab336e532e
jameel <jameel@republiccommandos.co.za>
2011-05-31 14:30:18 +0200

    Added admin link

--- spacemanager/admin.py
+++ spacemanager/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from fslm.spacemanager.models import Drive,MediaType,FolderType,Folder,FolderAdmin,DriveAdmin,FolderTypeAdmin,MediaTypeAdmin
+from fslm.spacemanager.models import 
 
 admin.site.register(Drive,DriveAdmin)
 admin.site.register(MediaType,MediaTypeAdmin)
 admin.site.register(Folder,FolderAdmin)
 admin.site.register(FolderType,FolderTypeAdmin)
+admin.site.register(MoveQueueItem)
