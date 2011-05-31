3c9578fb0a9fb50e97deb9915b9c6438752c4f19
hellola <ewoudt.kellerman@gmail.com>
2011-05-31 17:53:09 +1000

    added main.js to the repo

---
+++ spacemanager/static/js/main.js
@@ -0,0 +1,55 @@
+fslm = {
+drives : {
+	sizeData : {},
+	getDrivesSize: function(){
+	  $.get('/driveslist',function(data) {
+		eval('fslm.drives.sizeData = ' + data)
+		})
+	},
+	drawDrives: function(){
+         $('#loader').remove();
+		 for (i=0;i<fslm.drives.sizeData.length;i++) {
+			fslm.drives.drawDrive(fslm.drives.sizeData[i],i);
+			 //alert(fslm.drives.sizeData[i].name);
+			}
+	},
+    drawDrive: function(drve, index) {
+			 //var x = index*fslm.drives.drawXPadding + index*fslm.drives.drawWidth + fslm.drives.drawXPadding;
+             var x = fslm.drives.drawXPadding ;
+             var y = index*fslm.drives.drawYPadding + index*fslm.drives.drawHeight + fslm.drives.drawYPadding;
+			 var drive = fslm.paper.rect(x,y,fslm.drives.drawWidth,fslm.drives.drawHeight);
+			 drive.attr({stroke:"#000","stroke-width":"0.2",fill: "#DEDEDE","font-size":fslm.drives.drawFontSize});
+             var percentage = (drve.used/ drve.capacity) ;
+             var driveFull = fslm.paper.rect(x,y,fslm.drives.drawWidth*percentage,fslm.drives.drawHeight);
+             if (drve.isOver == "False")
+             {
+                driveFull.attr({fill:"#5689B3"});
+              }
+              else
+              {
+                  driveFull.attr({fill:"#D92B2B"});
+              }
+             fslm.paper.text(x+(fslm.drives.drawWidth*0.5),y+(fslm.drives.drawHeight/2),drve.name + " ("+Math.round(percentage* 100)+"%)");
+             
+    },
+    drawXPadding: 10,
+    drawYPadding: 10,
+	drawHeight: 40,
+	drawWidth: 100,
+    drawFontSize:12,
+	drawColour:'#efefef'
+},
+paper : {},
+initRaphael : function() {
+	fslm.paper = Raphael("drives",0,0,80,800);
+},
+init : function() {
+	fslm.drives.getDrivesSize();
+	fslm.initRaphael()
+    setTimeout('fslm.drives.drawDrives()',600);
+}
+
+}
+$(document).ready(function() { 
+fslm.init();
+});
