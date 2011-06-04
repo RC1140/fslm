3a34dda1bdc061259e8fb9fba830813df2b7bca5
jameel <jameel@republiccommandos.co.za>
2011-06-04 16:35:30 +0200

    DB Queue is now working and also allows for deletions

--- urls.py
+++ urls.py
@@ -2,13 +2,10 @@ from django.conf.urls.defaults import *
 import settings
 import os
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^fslm/', include('fslm.foo.urls')),
      (r'^move/', 'spacemanager.views.moveFiles'),
      (r'(^home/|^$)', 'spacemanager.views.home'), 
      (r'(^driveslist/)','spacemanager.views.drivesList'),
@@ -16,10 +13,6 @@ urlpatterns = patterns('',
      (r'^initDbQueue/$', 'spacemanager.views.initQueue'),
      url(r'^viewFileInDbQueue/$', 'spacemanager.views.viewDbQueue',name='dbQueue'),
      (r'^deleteQueueItem/(?P<queueid>\d+)/$', 'spacemanager.views.deleteQueueItem'),
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
      (r'^admin/', include(admin.site.urls)),
 )
 
